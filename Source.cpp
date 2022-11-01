3097c4bb5f46eba65b9dd82d00acf9715e642b30
Sm1leGitHub <113791071+Sm1leGitHub@users.noreply.github.com>
2022-11-01 18:41:55 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	//int mas[5];
+	////ввод
+	//for (int i = 0; i < 5; i++) {
+	//	cin >> mas[i];
+	//	//вывод
+	//	for (int i = 0; i < 5; i++) {
+	//		cout << mas[i] << " ";
+	//}
+	/*int mas[10];
+	for (int i = 0; i < 10; i++) {
+		cin >> mas[i];
+	}
+	for (int i = 0; i < 10; i++) {
+		cout << mas[i]<<" ";
+	}
+	int k = 0;
+	for (int i = 0; i < 10; i++) {
+		if (mas[i] > 0) k++;
+	}
+	cout << ">0" << k << endl;*/
+	//1
+	/*int mas[10];
+	for (int i = 1; i < 10; i++) {
+		cin >> mas[i];
+	}
+	for (int i = 9; i >= 0; i--) {
+		cout << "mas[i]" << " ";
+	}*/
+
+}
